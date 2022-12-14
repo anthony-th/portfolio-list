933f70352b5ac96778b64170e0b90c976ef91637
Anthony <yefremov.anthony@gmail.com>
2022-12-14 22:17:14 +0600

    rafactor: del contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/tailwindcss.css';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
