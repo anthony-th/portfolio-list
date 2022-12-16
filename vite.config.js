f1c6b58ccbba97feffa6c19a1f032d1779e75b05
Anthony <yefremov.anthony@gmail.com>
2022-12-16 22:43:18 +0600

    refactor: try to deploy with routes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/portfolio-list/`,
+  base: "/portfolio-list/",
   plugins: [react()],
-  define: {
-    "process.env.VITE_NAME": `"${process.env.VITE_NAME}"`,
-  },
 });
