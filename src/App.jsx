90a401bbcaff91c096b3129aa46a385fba5413d3
Anthony <yefremov.anthony@gmail.com>
2022-12-16 15:18:09 +0600

    feat: add outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Aboutpage from './pages/Aboutpage';
+import { Layout } from './components/Layout';
 
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/about' element={<Aboutpage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path='about' element={<Aboutpage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import '../styles/tailwindcss.css';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 import Portfolio from '../components/Portfolio';
 
 function Homepage() {
   return (
-    <div className="App bg-zinc-800 text-white min-w-[300px]">
-      <div className="max-w-6xl w-11/22 mx-auto flex flex-col justify-between min-h-screen">
-        <Header />
-        <Portfolio />
-        <Footer />
-      </div>       
-    </div>
+    <>
+      <Portfolio />
+    </>
   )
 }
 
