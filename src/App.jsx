ea0b7512c0887491c0d1a4223baaced9b7df3a03
Anthony <yefremov.anthony@gmail.com>
2022-12-16 23:19:49 +0600

    refactor: complete routers for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path='/portfolio-list/' element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path='about' element={<Aboutpage />} />
           <Route path='*' element={<Pagenotfound />} />
