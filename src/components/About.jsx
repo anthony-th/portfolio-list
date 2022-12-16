c55f1a707de1d4c22d5272a1b2b68b77d74bc347
Anthony <yefremov.anthony@gmail.com>
2022-12-16 23:38:05 +0600

    refactor: change about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-center font-yesteryear text-6xl py-8 text-[#ee1f80]">
-      A little bit later...
+      Soon...
     </div>
   )
 }
