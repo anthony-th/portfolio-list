3251c318784746592839646aaf31b25d4fff75ae
Anthony <yefremov.anthony@gmail.com>
2023-01-10 14:14:00 +0600

    refactor: add currentItems to useEffect

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,7 +15,7 @@ function Portfolio() {
 
   useEffect(() => {
     localStorage.setItem('activePage', currentPage);
-  }, [currentPage]);
+  }, [currentPage, currentItems]);
 
   return (
     <div className='flex flex-col gap-4 items-center justify-center'>
