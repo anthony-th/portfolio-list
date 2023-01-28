3a05913062dc03de41c2b9d07e2daef67f20f5a0
Anthony <yefremov.anthony@gmail.com>
2023-01-28 12:22:13 +0600

    feat: activate and change resumeButton

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import PdfImg from '/images/pdf.png';
 
 function ResumeButton() {
   return (
-    <a href='#' className='gap-1 flex items-center justify-center hover:text-[#ee1f80] transition-colors text-4xl font-yesteryear px-2 py-1'>
+    <a href='#' className='font-raleway hover:rounded-lg border border-[#ee1f80] px-3 py-1 rounded-sm hover:bg-[#ee1f80] transition-all duration-500'>
         <h2 className='flex items-center justify-center w-full tracking-wide'>
-          <span className='text-[#ee1f80]'>R</span>esume
+          Resume
         </h2>
-        <img
-          className='w-[27px] h-[27px]'
-          src={PdfImg}
-          alt='resume'
-          title='resume'
-        />
     </a>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ResumeButton from './ResumeButton';
 
 function Welcome() {
   return (
-    <div className='flex flex-col justify-center items-center max-w-[600px] gap-2 min-w-[300px]'>
+    <div className='flex flex-col justify-center items-center max-w-[600px] gap-4 min-w-[300px]'>
       <h1 className='font-raleway text-4xl text-center align-middle'>Hello! I'm&nbsp;
         <span className='text-[#ee1f80]'>Anthony</span>.<br />
         I'm a front-end web developer.
       </h1>
-      <NavLink to="/portfolio">
-        <button 
-          className='font-raleway hover:rounded-lg border border-[#ee1f80] px-3 py-1 rounded-sm hover:bg-[#ee1f80] transition-all duration-500'
-        >
-          Open Portfolio
-        </button>
-      </NavLink>
+      <div className="flex gap-4">
+        <NavLink to="/portfolio">
+          <button 
+            className='font-raleway hover:rounded-lg border border-[#ee1f80] px-3 py-1 rounded-sm hover:bg-[#ee1f80] transition-all duration-500'
+          >
+            Portfolio
+          </button>
+        </NavLink>
+        <ResumeButton />
+      </div>
     </div>
   )
 }
