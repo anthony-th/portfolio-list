3cd77f3b074f03c17f9ffc6bd99529ef4e773168
Anthony <yefremov.anthony@gmail.com>
2023-02-02 11:19:08 +0600

    feat: add animation to pagination

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import portfolio from '../data/portfolio';
 import { cardsContainer, cardsItem } from '../shared/types';
 import PortfolioItem from './PortfolioItem';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function Portfolio() {
   const itemsPerPage = 9;
@@ -19,42 +19,46 @@ function Portfolio() {
 
   return (
     <div className='w-fit flex flex-col gap-4 items-center justify-center'>
-      <motion.div
-        className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
-        variants={cardsContainer}
-        initial='hidden'
-        animate='visible'
-      >
-          {currentItems.map(project => (
-              <PortfolioItem
-                  key={project.title}
-                  imgUrl={project.imgUrl}
-                  title={project.title}
-                  stack={project.stack}
-                  projectUrl={project.projectUrl}
-                  deployUrl={project.deployUrl}
-              />
+      <AnimatePresence>
+        <motion.custom
+          key={currentPage}
+          className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+          variants={cardsContainer}
+          initial='hidden'
+          animate='visible'
+          exit={{ opacity: 0 }}
+        >
+            {currentItems.map(project => (
+                <PortfolioItem
+                    key={project.title}
+                    imgUrl={project.imgUrl}
+                    title={project.title}
+                    stack={project.stack}
+                    projectUrl={project.projectUrl}
+                    deployUrl={project.deployUrl}
+                />
+            ))}
+        </motion.custom>
+        <motion.div
+          className="flex justify-center items-center font-yesteryear text-xl"
+          variants={cardsContainer}
+          initial='hidden'
+          animate='visible'
+        >
+          <motion.button className={`border-[#ee1f80] border border-r-0 px-4 py-1 rounded-l ${currentPage === 1 ? 'text-gray-700 border-gray-700' : 'text-gray-300 hover:text-[#ee1f80] transition-colors'}`} onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} variants={cardsItem}>Previous</motion.button>
+          {Array.from({length: totalPages}, (_, i) => i + 1).map((pageNum, index) => (
+            <motion.button 
+              variants={cardsItem}
+              key={pageNum} 
+              className={`px-2 py-1 ${currentPage === pageNum ? 'text-[#ee1f80] border-[#ee1f80] cursor-default' : 'text-gray-300 hover:text-[#ee1f80] hover:border-[#ee1f80] transition-colors'} 
+              ${index === 0 ? 'pl-4' : ''} ${index === totalPages - 1 ? 'pr-4' : ''}`} 
+              onClick={() => setCurrentPage(pageNum)}>
+              {pageNum}
+            </motion.button>
           ))}
-      </motion.div>
-      <motion.div
-        className="flex justify-center items-center font-yesteryear text-xl"
-        variants={cardsContainer}
-        initial='hidden'
-        animate='visible'
-      >
-        <motion.button className={`border-[#ee1f80] border border-r-0 px-4 py-1 rounded-l ${currentPage === 1 ? 'text-gray-700 border-gray-700' : 'text-gray-300 hover:text-[#ee1f80] transition-colors'}`} onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} variants={cardsItem}>Previous</motion.button>
-        {Array.from({length: totalPages}, (_, i) => i + 1).map((pageNum, index) => (
-          <motion.button 
-            variants={cardsItem}
-            key={pageNum} 
-            className={`px-2 py-1 ${currentPage === pageNum ? 'text-[#ee1f80] border-[#ee1f80] cursor-default' : 'text-gray-300 hover:text-[#ee1f80] hover:border-[#ee1f80] transition-colors'} 
-            ${index === 0 ? 'pl-4' : ''} ${index === totalPages - 1 ? 'pr-4' : ''}`} 
-            onClick={() => setCurrentPage(pageNum)}>
-            {pageNum}
-          </motion.button>
-        ))}
-        <motion.button className={`border-[#ee1f80] border border-l-0 px-4 py-1 rounded-r ${currentPage === totalPages ? 'text-gray-700 border-gray-700' : 'text-gray-300 hover:text-[#ee1f80] transition-colors'}`} onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages} variants={cardsItem}>Next</motion.button>
-      </motion.div>
+          <motion.button className={`border-[#ee1f80] border border-l-0 px-4 py-1 rounded-r ${currentPage === totalPages ? 'text-gray-700 border-gray-700' : 'text-gray-300 hover:text-[#ee1f80] transition-colors'}`} onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages} variants={cardsItem}>Next</motion.button>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
