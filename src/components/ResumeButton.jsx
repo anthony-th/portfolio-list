46a53e82a48d6c6c5ceca351a2ab0191958310bc
Anthony <yefremov.anthony@gmail.com>
2023-02-06 17:13:33 +0600

    refactor: link cv edit

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResumeButton() {
   return (
-    <a href='https://drive.google.com/file/d/1blVQ4FvVogoVHk8lpdO7F7kkLqNjV2ks/view?usp=share_link' className='font-raleway hover:rounded-lg border border-[#ee1f80] px-3 py-1 rounded-sm hover:bg-[#ee1f80] transition-all duration-500' target="_blank" rel="noopener noreferrer">
+    <a href='https://drive.google.com/file/d/1WkM_TYCnzl7AHm-vGC9xdZPvZefjAoaU/view' className='font-raleway hover:rounded-lg border border-[#ee1f80] px-3 py-1 rounded-sm hover:bg-[#ee1f80] transition-all duration-500' target="_blank" rel="noopener noreferrer">
         <h2 className='flex items-center justify-center w-full tracking-wide'>
           Resume
         </h2>
