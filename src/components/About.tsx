42427031ee2d417de50778a0777ab0e3bdfd717a
Anthony <yefremov.anthony@gmail.com>
2023-03-03 15:05:23 +0600

    refactor: del state from about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { cardsContainer, cardsItem } from "../shared/types";
 import { MdSend } from "react-icons/md";
